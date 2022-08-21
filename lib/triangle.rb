3705345f134f930d027c48d04c7097c810d6f0ac
bonveeee <bonochieng@gmail.com>
2022-08-21 07:58:08 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  # write side_code here
+  attr_accessor :side_a, :side_side_b, :side_side_c
+
+  def initialize (side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+
+  end
+
+  def kind
+      if @side_a <= 0 ||  @side_b <= 0 || @side_c <= 0
+        raise TriangleError
+      end 
+
+      if @side_a + @side_b <= @side_c  || @side_a + @side_c <= @side_b ||  @side_b + @side_c <= @side_a
+        raise TriangleError
+      end 
+
+      return :equilateral  if @side_a == @side_b && @side_b == @side_c
+      return :isosceles   if @side_a == @side_b || @side_a == @side_c ||  @side_b == @side_c
+      return :scalene     if @side_a != @side_b && @side_a != @side_c &&  @side_b != @side_c 
+
+    end
+ 
+ 
+    class TriangleError < StandardError
+      def message
+        "you must give the correct arguments of an instance of the Triangle class!"
+      end
+    end
 end
